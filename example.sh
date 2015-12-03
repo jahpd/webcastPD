4ff1fb068d04b8801fd2a374de8f02a45cbf0e2a
jahpd <gcravista@gmail.com>
2015-12-03 15:43:02 -0200

    atualizado example.sh

--- example.sh
+++ example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py -H 10.42.0.1 -p 8000 -l listen.ogg -m /home/guilherme/Github/labic_patchs/music/ -x hackme
+./webcast -H 10.42.0.1 -p 8000 -l listen.ogg -m Musics -x hackme
